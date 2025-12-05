e5c2693250170d80041fabc4d7cb879e1e6e765c
thelastvascaino <lincolncdelfino@gmail.com>
2025-12-05 17:21:59 -0300

    up

---
+++ exercicios_curso/processamentoDados.java
@@ -0,0 +1,11 @@
+public class processamentoDados {
+    public static void main(String[] args) {  
+        //ex (base + BASE) / 2 * altura(h)
+        double b = 6.0;
+        double B = 8.0;
+        double h = 5.0;
+        double trapezio = (b + B) / 2 * h;
+
+        System.out.println("Area do trapezio eh: " + trapezio);
+    }
+}
