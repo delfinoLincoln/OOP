fd17c15b2dcff5f4819aa780f8e1fcd3018a9b44
thelastvascaino <lincolncdelfino@gmail.com>
2025-12-07 12:29:59 -0300

    update

--- funcoes_basicas/entradaDados.java
+++ funcoes_basicas/entradaDados.java
@@ -16,31 +16,35 @@ public class entradaDados {
         
         System.out.print("Digite seu nome: ");
         nome = sc.nextLine();
-        System.out.println("Nome: " + nome);
 
         System.out.print("Digite sua idade: ");
         idade = sc.nextInt();
         sc.nextLine();
-        System.out.println("Idade: " + idade);
 
         System.out.print("Nota 1 bimestre: ");
         nota1 = sc.nextDouble();
         sc.nextLine();
-        System.out.println("Nota: " + nota1);
-
+        
         System.out.print("Nota 2 bimestre: ");
         nota2 = sc.nextDouble();
         sc.nextLine();
-        System.out.println("Nota: " + nota2);
-
+        
         System.out.print("Nota 3 bimestre: ");
         nota3 = sc.nextDouble();
         sc.nextLine();
+        
+        System.out.println();
+        System.out.println("---Visao Geral---");
+        System.out.println("----------------");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Nota: " + nota1);
+        System.out.println("Nota: " + nota2);
         System.out.println("Nota: " + nota3);
-
+        System.out.println("----------------");
         media = (nota1 + nota2 + nota3) / 3;
         System.out.printf("Media final: %.2f\n", media);
-
+        
         if (media > 7.0) {
             System.out.println("Voce passou!!!");
         }
@@ -50,6 +54,9 @@ public class entradaDados {
         else {
             System.out.println("Voce esta de recuperacao!!!");
         }
+        
+
+        
         sc.close();
     }
 }
