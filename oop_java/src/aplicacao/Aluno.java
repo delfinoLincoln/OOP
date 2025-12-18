930259634a50aedb8a73a79824c2e2f540e114b7
thelastvascaino <lincolncdelfino@gmail.com>
2025-12-18 10:27:56 -0300

    update

---
+++ oop_java/src/aplicacao/Aluno.java
@@ -0,0 +1,45 @@
+package aplicacao;
+import entidades.NotaAluno;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aluno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        sc.useLocale(Locale.US);
+
+        NotaAluno aluno = new NotaAluno();
+
+        System.out.println("---SISTEMA DA ESCOLA---");
+        System.out.print("Digite o nome do aluno: ");
+        aluno.nome = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Digite a nota do bimestre 1: ");
+        aluno.bim1 = sc.nextDouble();
+        System.out.println();
+
+        System.out.println("Digite a nota do bimestre 2: ");
+        aluno.bim2 = sc.nextDouble();
+        System.out.println();
+
+        System.out.println("Digite a nota do bimestre 3: ");
+        aluno.bim2 = sc.nextDouble();
+        System.out.println();   
+
+        System.out.println("---RESULTADO FINAL---");
+        System.out.printf("%.2f\n", aluno.resultadoFinal());
+
+        if(aluno.resultadoFinal() > 60.0) {
+            System.out.println("ALUNO REPROVADO!!!");
+            System.out.printf("FALTARAM %.2f pontos\n", aluno.faltaPontos());
+        }
+        else {
+            System.out.println("ALUNO APROVADO!!!");
+        }
+
+        sc.close();
+    }
+}
---
+++ oop_java/src/aplicacao/AreaRetangulo.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import entidades.Retangulo;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class AreaRetangulo {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        Retangulo retangulo = new Retangulo();
+
+        System.out.print("Digite a altura do retangulo: ");
+        retangulo.altura = sc.nextDouble();
+
+        System.out.print("Digite a largura do retangulo: ");
+        retangulo.largura = sc.nextDouble();
+
+        System.out.printf("Area: %.2f\n", retangulo.area());
+        System.out.printf("Perimetro: %.2f\n", retangulo.perimetro());
+        System.out.printf("Diagonal: %.2f\n", retangulo.diagonal());
+
+        sc.close();
+    }
+}
