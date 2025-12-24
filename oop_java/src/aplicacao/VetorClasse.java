b5f86750cfca404179c1b1b2e5e850c03546c340
thelastvascaino <lincolncdelfino@gmail.com>
2025-12-23 22:02:18 -0300

    up

---
+++ oop_java/src/aplicacao/VetorClasse.java
@@ -0,0 +1,42 @@
+package aplicacao;
+
+import entidades.VetProduct;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class VetorClasse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite um valor: ");
+        int n = sc.nextInt();
+        
+        VetProduct[] produto = new VetProduct[n];
+        
+        for(int i = 0; i < produto.length; i++) {
+            sc.nextLine();
+            System.out.printf("Insira o nome do produto [%d]: ", i + 1);
+            String nome = sc.nextLine();
+            System.out.printf("Insira o preco do produto [%d]: ", i + 1);
+            Double preco = sc.nextDouble(); 
+            produto[i] = new VetProduct(nome, preco);
+        }
+
+        double soma = 0.0;
+        for(int i = 0; i < produto.length; i++) {
+            soma += produto[i].getPreco();
+        }
+
+        System.out.println("---DADOS DOS PRODUTOS---");
+        for(int i = 0; i < produto.length; i++) {
+            System.out.println(produto[i]);
+        }
+
+        double mediaFinal = soma / produto.length;
+        System.out.printf("Media de precos: %.2f", mediaFinal);
+        
+        sc.close();
+    }
+}
---
+++ oop_java/src/aplicacao/VetorValor.java
@@ -0,0 +1,35 @@
+package aplicacao;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class VetorValor {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        sc.useLocale(Locale.US);
+
+        System.out.print("Insira um valor: ");
+        int n = sc.nextInt();
+        double[] altura = new double[n];
+        System.out.println();
+        System.out.printf("Insira %d valores\n", n);
+
+        for(int i = 0; i < n; i++) {
+            System.out.printf("Indice [%d]: ", i);
+            altura[i] = sc.nextDouble();    
+        }
+
+        double soma = 0.0;
+        for(int i = 0; i < n; i++) {
+            soma += altura[i];
+        }
+
+        double alturaMedia = soma / n;
+        System.out.printf("Media de alturas: %.2f", alturaMedia);
+
+
+        sc.close();
+    }
+}
