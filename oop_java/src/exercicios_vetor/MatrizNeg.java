993aff70fabfef8a622561458aade90675e0783c
thelastvascaino <lincolncdelfino@gmail.com>
2025-12-27 00:04:54 -0300

    up

--- oop_java/src/exercicios_vetor/MatrizNeg.java
+++ oop_java/src/exercicios_vetor/MatrizNeg.java
@@ -6,23 +6,27 @@ public class MatrizNeg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um numero: ");
+        System.out.print("Digite o tamanho da matriz (L x C): ");
         int n = sc.nextInt();
 
         int[][] matriz = new int[n][n];
 
+        System.out.println("\nPreencha a matriz " + n + "x" + n + ":");
+        
         for(int i = 0; i < n; i++) {
-            System.out.print("Digite um valor para linha: ");
             for(int j = 0; j < n; j++) {
-                System.out.println("Digite um valor para coluna: ");
+                System.out.printf("Posição [%d][%d]: ", i, j);
                 matriz[i][j] = sc.nextInt();
             }
         }
 
+        System.out.println("\n=== MATRIZ DIGITALIZADA ===");
+        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                System.out.println(matriz[i][j]);
+                System.out.print(matriz[i][j] + "\t");
             }
+            System.out.println();
         }
 
         sc.close();
