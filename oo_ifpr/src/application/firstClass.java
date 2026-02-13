eea6e061fd801d3868bb04d7342dc3034656c37e
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-13 15:19:57 -0300

    first app in OO class

---
+++ oo_ifpr/src/application/firstClass.java
@@ -0,0 +1,10 @@
+package application;
+
+import util.ClearScreen;
+
+public class firstClass {
+    public static void main(String[] args) {
+        ClearScreen.cls();
+        System.out.println("Hello world");
+    }
+}
